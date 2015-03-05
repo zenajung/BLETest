f2ea720d304347e0517675b700613bda1dd28baa
Zena Jung <zenajung@paran.com>
2015-03-05 11:19:56 +0900

    Initial Commit

---
+++ BLETest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLETest
+//
+//  Created by   zenajung on 2015. 3. 5..
+//  Copyright (c) 2015년   zenajung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLETest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLETest
+//
+//  Created by   zenajung on 2015. 3. 5..
+//  Copyright (c) 2015년   zenajung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
